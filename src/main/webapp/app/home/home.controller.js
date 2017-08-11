606405e2ba559ffe06de408d3676678e5da7770b
751249871@qq.com <751249871@qq.com>
2017-08-11 08:47:49 +0800

    新增首页

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -8,9 +8,11 @@
     HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
 
     function HomeController ($scope, Principal, LoginService, $state) {
-        
-        $(".swiper-container").luara({interval:3000,selected:"seleted",deriction:"left"});
+    	
+        $(".swiper-container").luara({width:"1519.2",height:"500",interval:3000,selected:"seleted",deriction:"left"});
+    	
         var vm = this;
+
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
@@ -30,17 +32,5 @@
         function register () {
             $state.go('register');
         }
-        /*$("#indicators li").click(function(){
-            $(this).addClass("carouse_style");
-        })*/
-
-        $scope.course=[
-            {url:"content/images/2.png",text:"EduSoho慕课版介绍"},
-            {url:"content/images/3.jpg",text:"EduSoho慕课版介绍"},
-            {url:"content/images/4.jpg",text:"EduSoho慕课版介绍"},
-            {url:"content/images/5.jpg",text:"EduSoho慕课版介绍"},
-            {url:"content/images/6.jpg",text:"EduSoho慕课版介绍"},
-            {url:"content/images/7.jpg",text:"EduSoho慕课版介绍"}
-        ]
     }
 })();
